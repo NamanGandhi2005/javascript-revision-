452fa7756754ba61899be80246e9f14e01f924cc
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-05-31 18:24:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId=12323;//cont value cannot be changed
+let accountEmail="namangandhipersonal@gmail.com";//let value acan be cchanged
+
+var accountPassword="2323";
+// accountId=2//not allowed
+accountCity="Jaipur";
+let accountState;//if we do not declare any value then it is undefined
+
+
+accountEmail="asdads.com";
+
+accountPassword="242242";
+
+accountCity="begalurur";
+
+
+console.log(accountId);
+
+/*
+multiline comment 
+for not using var instead of let 
+
+beacuse of issue in block scope and func scope
+
+*/
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"//treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+let name="naman";
+let age=20;
+let state=null;
+let isLoggedIn=false;
+
+//number => 2 to power 53
+//bigint very large number 
+//string => ""
+//boolean => true or false
+//null => standalone value 
+//undefined => value assign nhi hui 
+// symbol => unique
+
+
+//object 
+
+console.log(typeof(undefined));

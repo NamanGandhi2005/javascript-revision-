8c9cf2b9402e0b65b3f1432f01b593be8e8842c7
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-06-01 17:39:28 +0000

    datatypesconversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score="33";
+let score2="33a";//Not a Number NaN
+let score3=null;
+
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score);//to convert string in Number
+let valueInNumber2=Number(score2);//to convert string in Number
+let valueInNumber3=Number(score3);//to convert string in Number
+
+console.log(typeof(valueInNumber));
+console.log(typeof(valueInNumber2));
+console.log(typeof(valueInNumber3));
+
+console.log(valueInNumber2);
+console.log(valueInNumber3);
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+//1->true 0=> false
+//" "=>false
+//"hitesh"=> true
+
+let someNumber=33;
+
+let stringNumber=String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+

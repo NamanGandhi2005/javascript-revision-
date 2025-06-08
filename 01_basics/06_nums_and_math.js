0691b5ccab0d1a464b30c6b26ec389dd25fcfa09
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-06-08 18:59:13 +0000

    updatae

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -20,6 +20,31 @@ const hundreds=10000000
 console.log(hundreds.toLocaleString());//print us standarad values
 console.log(hundreds.toLocaleString('en-IN'));//print indian standarad values
 
+//+++++++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));//also in cpp same 
+console.log(Math.round(4.6));//default round off 
+
+console.log(Math.ceil(4.6));//always top round off returns 5
+console.log(Math.floor(4.9));//always top round off returns 5
+
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+console.log(Math.random());//genrates random value bbt
+
+console.log(Math.floor(Math.random()*10+1));//genbrate randomwe value between 0 to 10
+
+
+const min=10
+const max=20
+
+//i want random values between 10 and 20 
+
+console.log((Math.random()*(max-min+1)+min));
+
+
 
 
 

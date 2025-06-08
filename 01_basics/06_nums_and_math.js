d3e74ce450f153e811eb30a995d13fb8e02eec77
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-06-08 16:38:55 +0000

    update

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+const score=400;
+
+const balance =new Number(100)//object define that is number type
+
+console.log(balance);
+console.log(balance.toString());
+console.log(typeof(balance.toString()));
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(1));//100.0
+
+
+
+const otherNumber=23533.8966;
+
+console.log(otherNumber.toPrecision(3));//2.35e+4
+
+const hundreds=10000000
+
+console.log(hundreds.toLocaleString());//print us standarad values
+console.log(hundreds.toLocaleString('en-IN'));//print indian standarad values
+
+
+
+
+

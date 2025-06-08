c571945f50d2d614d6fd6bc41456095105e336d4
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-06-08 07:41:03 +0000

    update

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -45,4 +45,27 @@ let value=3;
 let negvalue=-value;
 console.log(typeof(value));
 
+let str1="hello";
+let str2="naman";
+
+let str3=str1+str2;
+
+console.log(str3);
+
+console.log("1"+2);//op=12
+console.log(1+"2");//opt=12
+console.log("2"+1+1);//opt=211
+console.log(1+1+"2");//opt=22
+
+console.log(+true);//op=1
+console.log(+"");//op=1
+
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+
+
 

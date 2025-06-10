6304f8ec973cab8b8f3d9e61240a4a27391e82c7
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-06-10 08:29:07 +0000

    update

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,54 @@
+//arrays declaration types 
+
+
+const myarr=[1,32,4,5];
+
+const myHeroes=["thor","hulk"];
+
+const myArr2=new Array(1,2,4,5,5);
+console.log(myarr);
+console.log(myHeroes);
+console.log(myArr2);
+
+
+//+++++++++++++++++++++++arrays methods++++++++++
+
+
+myarr.push(6);//adds value at end of array
+console.log(myarr);
+
+myarr.pop()//removes last element from array
+
+
+myarr.unshift(0);//adds element to the starting of array 
+
+console.log(myarr);
+
+myarr.shift();//removes first element from array
+console.log(myarr);
+
+console.log(myarr.includes(9));//return true ot false
+
+console.log(myarr.indexOf(4));//returns index of element you want
+
+const newArr=myarr.join();
+
+console.log(myarr);
+console.log(newArr);//return type of string comma separated
+
+//slice ,splice 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
